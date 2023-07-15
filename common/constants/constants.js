045d9d5000c85d11a99b8683b3483279765628ae
ducan <ducan172002@gmail.com>
2023-07-16 02:47:34 +0700

    [TP-28]- migrate schema

--- common/constants/constants.js
+++ common/constants/constants.js
@@ -1,1 +1,5 @@
 // TODO: Define app constants here
+const STATUS = Object.freeze({
+    ACTIVE: 1,
+    INACTIVE: 0,
+});
---
+++ internal/models/booking/const.js
@@ -0,0 +1,12 @@
+// TODO: Handle status codes
+// Booking Status
+const BOOKING_STATUS = Object.freeze({
+    PENDING: 'PENDING',
+    CONFIRMED: 'CONFIRMED',
+    COMPLETED: 'COMPLETED',
+    CANCELLED: 'CANCELLED'
+});
+
+// Other Constants
+const MAX_CONFIRM_TIME = 10;
+const DEFAULT_BOOKING_TIME = 15; // in minutes
