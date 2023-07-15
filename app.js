1737addd9f6a77fe55ce5b829972387845bb0a76
nminhdev <heomeco2014@gmail.com>
2023-07-16 01:18:01 +0700

    feat: adding authorization

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 // Import Mongoose
 const mongoose = require('mongoose');
+const Role = require('./models/Role');
 var app = express();
 app.use(cors());
 initializeDB();
@@ -30,10 +31,29 @@ async function initializeDB() {
     })
     .then(() => {
       console.log('Connected to MongoDB');
+      initial();
     })
     .catch((error) => {
       console.error('Error connecting to MongoDB:', error);
+      process.exit();
     });
+}
+function initial() {
+  Role.estimatedDocumentCount().then((count) => {
+    if (count === 0) {
+      const createUserRole = new Role({ name: 'user' }).save();
+      const createDriverRole = new Role({ name: 'driver' }).save();
+      const createAdminRole = new Role({ name: 'admin' }).save();
+
+      Promise.all([createUserRole, createDriverRole, createAdminRole])
+        .then(() => {
+          console.log("Added 'user', 'driver', and 'admin' roles to the roles collection");
+        })
+        .catch((err) => {
+          console.error('Error adding roles:', err);
+        });
+    }
+  });
 }
 
 module.exports = app;
--- models/User.js
+++ models/User.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
 const UserSchema = new Schema({
-	name: String,
-	phoneNumber: String,
-	email: String,
-	password: String,
+  name: String,
+  phoneNumber: String,
+  email: String,
+  password: String,
+  roles: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Role'
+    }
+  ]
 });
 
 const UserModel = mongoose.model('User', UserSchema);
